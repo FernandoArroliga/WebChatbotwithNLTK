a1ef9be0830e3513bf64ec4dbd2d46d9914b370e
Fernando Arroliga <FernandoArroliga@hotmail.com>
2023-09-26 07:25:47 -0600

    "Organizing the chat code"

--- chat.py
+++ chat.py
@@ -7,13 +7,13 @@ from nltk_utils import bag_of_words, tokenize
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-with open("intents.json", "r") as f:
-    intents = json.load(f)
+with open("intents.json", "r") as json_file:
+    intents = json.load(json_file)
     
 FILE = "data.pth"
 data = torch.load(FILE)
 
-# Save some data
+# Variables
 input_size = data["input_size"]    
 hidden_size = data["hidden_size"]    
 output_size = data["output_size"] 
@@ -23,38 +23,46 @@ model_state = data["model_state"]
 
 model = NeuralNet(input_size, hidden_size, output_size).to(device) 
 model.load_state_dict(model_state)
-model.eval()
-
-# Creating the chat
-bot_name = "Eva"
-print("Let´s chat! type 'quit' to exit")
-
-while True:
-    sentence = input("You: ")
+model.eval()         
+        
+bot_name = "Eva"        
+        
+def get_response(msg):
+    """ 
+    Function to create the chatbot
     
-    if sentence == "quit":
-        break
-    
-    # tokenize the sentence
-    sentence = tokenize(sentence)
+    """
+    sentence = tokenize(msg)
     x = bag_of_words(sentence, all_words)
     x = x.reshape(1, x.shape[0])
-    x = torch.from_numpy(x)
+    x = torch.from_numpy(x).to(device)
     
     output = model(x)
     _, predicted = torch.max(output, dim=1)
+    
     tag = tags[predicted.item()]
     
-    # calculate the probabilities
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
     
-    # considering the probability to generate the response
     if prob.item() > 0.75:
-            
         for intent in intents["intents"]:
             if tag == intent["tag"]:
-                print(f'{bot_name}: {random.choice(intent["responses"])}')
+                return random.choice(intent["responses"])
+            
+    return "I do not understand..."
+
+# Main Program
+
+if __name__ == "__main__":
     
-    else:            
-        print(f"{bot_name}: I do not understand...")          
+    print("Let's chat! (type 'quit' to exit)")
+    while True:
+
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
+
+        resp = get_response(sentence)
+        print(resp)
+
